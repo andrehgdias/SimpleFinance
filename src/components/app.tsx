3696d4b06245b225bd32ce205a399e5a3f63dcb6
André Dias <andredias@tuta.io>
2026-02-22 13:55:33 +0100

    chore: Setup Solid
    Closes #25

---
+++ src/components/app.tsx
@@ -0,0 +1,4 @@
+import type { Component } from "solid-js"
+
+const App: Component = () => <h1>Hello</h1>
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import "./style.css"
+import { render } from "solid-js/web"
+import App from "./components/app.tsx"
+
+const root = document.getElementById("root")
+
+if (!root) {
+  throw new Error("Root element is missing")
+}
+render(() => <App />, root)
