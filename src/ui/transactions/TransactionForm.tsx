1cc4716540789f1fd0cc1847927369fc0e432ae0
André Dias <andredias@tuta.io>
2026-03-13 19:24:33 +0100

    feat: Finish TransactionForm
    Closes #15 and #11

--- src/ui/transactions/TransactionForm.tsx
+++ src/ui/transactions/TransactionForm.tsx
@@ -2,6 +2,8 @@ import styles from "./style.module.css"
 import { TransactionFormViewModel } from "./TransactionFormViewModel.ts"
 import TransactionService from "../../application/services/TransactionService.ts"
 import type { Component, JSX } from "solid-js"
+import { TransactionType } from "../../domain/entities/Transaction.ts"
+import { Currency } from "../../domain/value-objects/Money.ts"
 
 export type TransactionFormProps = {
   transactionService: TransactionService
@@ -25,6 +27,10 @@ const TransactionForm: Component<TransactionFormProps> = props => {
     viewModel.setDate(e.currentTarget.value)
   }
 
+  const handleSetType = (type: TransactionType) => {
+    viewModel.setType(type)
+  }
+
   const handleSubmit = async (e: MouseEvent) => {
     e.preventDefault()
     if (await viewModel.submit()) {
@@ -34,38 +40,84 @@ const TransactionForm: Component<TransactionFormProps> = props => {
 
   return (
     <form class={styles["transaction-form"]}>
-      <label for="amount">Amount</label>
-      <input
-        type="number"
-        name="amount"
-        id="amount"
-        min="0"
-        value={viewModel.state.draft.amount}
-        onInput={handleSetAmount}
-      />
+      <ol>
+        <li>
+          <input
+            type="radio"
+            name="type"
+            id="income"
+            onInput={() => handleSetType(TransactionType.INCOME)}
+            checked={viewModel.state.draft.type === TransactionType.INCOME}
+          />
+          <label for="income">Income</label>
+        </li>
+        <li>
+          <input
+            type="radio"
+            name="type"
+            id="expense"
+            onInput={() => handleSetType(TransactionType.OUTCOME)}
+            checked={viewModel.state.draft.type === TransactionType.OUTCOME}
+          />
+          <label for="expense">Expense</label>
+        </li>
+      </ol>
 
-      <label for="description">Description</label>
-      <input
-        type="text"
-        name="description"
-        id="description"
-        value={viewModel.state.draft.description}
-        onInput={handleSetDescription}
-      />
+      <div>
+        <div>
+          <select name="currency" id="currency" disabled>
+            <option value={Currency.EUR}>EUR</option>
+          </select>
+        </div>
+        <label for="amount">Amount</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min="0"
+          value={viewModel.state.draft.amount}
+          onInput={handleSetAmount}
+        />
+        <span class={styles["field-error"]}>{viewModel.state.errors.fields.amount || ""}</span>
+      </div>
 
-      <label for="date">Date</label>
-      <input
-        type="date"
-        name="date"
-        id="date"
-        max={maxDateISO}
-        value={viewModel.state.draft.date}
-        onInput={handleSetDate}
-      />
+      <div>
+        <label for="description">Description</label>
+        <input
+          type="text"
+          name="description"
+          id="description"
+          value={viewModel.state.draft.description}
+          onInput={handleSetDescription}
+        />
+        <span class={styles["field-error"]}>{viewModel.state.errors.fields.description || ""}</span>
+      </div>
 
-      <button type="submit" onClick={handleSubmit}>
-        Submit
-      </button>
+      <div>
+        <label for="date">Date</label>
+        <input
+          type="date"
+          name="date"
+          id="date"
+          max={maxDateISO}
+          value={viewModel.state.draft.date}
+          onInput={handleSetDate}
+        />
+        <span class={styles["field-error"]}>{viewModel.state.errors.fields.date || ""}</span>
+      </div>
+
+      <div>
+        <span class={styles["form-error"]}>{viewModel.state.errors.formError}</span>
+        {/*TODO Remove error report below*/}
+        <span class={styles["form-error"]}>{viewModel.state.errors.lastSubmitErrorMessage}</span>
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          disabled={!viewModel.isValid() || viewModel.isSubmitting()}
+        >
+          Submit
+        </button>
+      </div>
     </form>
   )
 }
