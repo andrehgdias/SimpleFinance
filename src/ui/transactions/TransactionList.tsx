dd8ef8dca8dfee32f42e044d9c498dfe1eff57c9
André Dias <andredias@tuta.io>
2026-03-21 08:20:31 +0100

    feat: UI Delete delete transactions
    Closes #24

--- src/ui/transactions/TransactionList.tsx
+++ src/ui/transactions/TransactionList.tsx
@@ -1,9 +1,11 @@
 import { type Accessor, type Component, createResource, ErrorBoundary, For, Show } from "solid-js"
 import type TransactionService from "../../application/services/TransactionService.ts"
+import NotFoundError from "../../application/errors/NotFoundError.ts"
 
 export type TransactionListProps = {
   transactionService: TransactionService
   refreshTrigger: Accessor<number>
+  onDeleteTransaction: () => void
 }
 
 const TransactionList: Component<TransactionListProps> = props => {
@@ -11,14 +13,31 @@ const TransactionList: Component<TransactionListProps> = props => {
     props.transactionService.getAllTransactions(),
   )
 
+  const handleDelete = async (e: MouseEvent, id: string) => {
+    e.preventDefault()
+
+    try {
+      await props.transactionService.deleteTransaction(id)
+      props.onDeleteTransaction()
+    } catch (e) {
+      if (e instanceof NotFoundError) {
+        console.warn("Transaction not found, refreshing list.\n\n", e)
+        props.onDeleteTransaction() // Refresh anyway
+      } else {
+        console.error("Unexpected error deleting transaction\n\n", e)
+      }
+    }
+  }
+
   return (
-    <ErrorBoundary fallback={<div>Error</div>}>
+    <ErrorBoundary fallback={<div>Error loading transactions</div>}>
       <Show when={!transactions.loading} fallback={<div>Loading...</div>}>
         <ol>
           <For each={transactions.latest ?? []}>
             {transaction => (
               <li>
                 {`${transaction.description} - ${transaction.type} ${transaction.amount.format()} at ${transaction.date.toLocaleString()}`}
+                <button onClick={e => handleDelete(e, transaction.id)}>X</button>
               </li>
             )}
           </For>
